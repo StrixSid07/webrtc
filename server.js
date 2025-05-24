3906ec976ebb862f071b62d8bbc4939fabf79018
Genius Graphics & Infotech <geniusgraphicsdev07@gmail.com>
2025-05-24 17:33:42 +0530

    commit

--- server.js
+++ server.js
@@ -4,42 +4,50 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(express.static("public"));
-
 const PORT = process.env.PORT || 3000;
+
+const roomHosts = new Map(); // track host socket per room
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("join-room", ({ roomId }) => {
-    socket.join(roomId);
-    socket.roomId = roomId; // Track room per socket
-    console.log(`${socket.id} joined room ${roomId}`);
+  socket.on("join-room", ({ roomId, isHost }) => {
+    const hostId = roomHosts.get(roomId);
 
+    if (isHost && hostId) {
+      socket.emit("host-exists");
+      return;
+    }
+
+    socket.join(roomId);
+    socket.roomId = roomId;
+    socket.isHost = isHost;
+
+    if (isHost) {
+      roomHosts.set(roomId, socket.id);
+    }
+
+    // Get all clients in room excluding current socket
     const clients = [...(io.sockets.adapter.rooms.get(roomId) || [])];
     const otherUsers = clients.filter((id) => id !== socket.id);
 
-    // Notify new user about others
     socket.emit("existing-users", otherUsers);
-
-    // Notify existing users about new one
     socket.to(roomId).emit("user-joined", socket.id);
   });
 
   socket.on("offer", (data) => {
-    socket.to(data.to).emit("offer", {
-      from: socket.id,
-      sdp: data.sdp,
-    });
+    socket.to(data.to).emit("offer", { from: socket.id, sdp: data.sdp });
   });
 
   socket.on("answer", (data) => {
-    socket.to(data.to).emit("answer", {
-      from: socket.id,
-      sdp: data.sdp,
-    });
+    socket.to(data.to).emit("answer", { from: socket.id, sdp: data.sdp });
   });
 
   socket.on("ice-candidate", (data) => {
@@ -49,9 +57,17 @@ io.on("connection", (socket) => {
     });
   });
 
+  socket.on("stop-stream", () => {
+    socket.to(socket.roomId).emit("stop-stream");
+  });
+
   socket.on("disconnect", () => {
     const roomId = socket.roomId;
-    if (roomId) {
+
+    if (socket.isHost) {
+      roomHosts.delete(roomId);
+      socket.to(roomId).emit("host-disconnected");
+    } else {
       socket.to(roomId).emit("user-left", socket.id);
     }
     console.log("User disconnected:", socket.id);
@@ -59,5 +75,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
